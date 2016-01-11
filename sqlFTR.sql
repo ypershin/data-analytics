87671a15bd587731de3e2c806c64957e640cc7ef
Yury <ypershin114@gmail.com>
2016-01-11 16:45:21 -0700

    Update sqlFTR.sql

--- sqlFTR.sql
+++ sqlFTR.sql
@@ -15,6 +15,9 @@ with (
 	tablock
 );
 
+select top 10 * from FtrNodes
+order by pNodeId;
+
 -- drop index iFtrNodes on FtrNodes;
 -- create nonclustered index iFtrNodes on FtrNodes(pNodeId);
 
@@ -38,7 +41,7 @@ with (
 	tablock
 );
 
-select max(pNodeId) from NodeZone;
+select top 10 * from NodeZone;
 
 drop table FtrMcp;
 select * into FtrMcp from (
@@ -56,6 +59,8 @@ select nodeId, month_,
 
 -- select top 10 * from FtrMcp;
 
+
+create nonclustered index iFtrMcp on FtrMcp(month_) include(nodeId,prcPk,prcOpk);
 
 drop table PriceMonthly2011;
 drop table PriceMonthly2012;
@@ -137,11 +142,67 @@ drop table SummaryData;
 select * into SummaryData from (
 select a.pNodeId srcId, b.pNodeId sinkId, 
 count(a.period) cnt,
-round(avg(b.revDaPk-a.revDaPk),2) avgRev, 
-min(b.revDaPk-a.revDaPk) minRev,
-max(b.revDaPk-a.revDaPk) maxRev,
-round(stdev(b.revDaPk-a.revDaPk),2) sdRev
+round(avg(b.revDaPk-a.revDaPk),2) avgPk, 
+min(b.revDaPk-a.revDaPk) minPk,
+max(b.revDaPk-a.revDaPk) maxPk,
+round(stdev(b.revDaOpk-a.revDaOpk),2) sdPk,
+round(avg(b.revDaOpk-a.revDaOpk),2) avgOpk, 
+min(b.revDaOpk-a.revDaOpk) minOpk,
+max(b.revDaOpk-a.revDaOpk) maxOpk,
+round(stdev(b.revDaOpk-a.revDaOpk),2) sdOpk
 from PriceMonthly a, PriceMonthly b
 where a.period=b.period
 and a.pNodeId<>b.pNodeId
+-- and a.pNodeId<48650 and b.pNodeId<48650
 group by a.pNodeId,b.pNodeId) z;
+
+create nonclustered index iSummaryDataSrcSink on SummaryData(srcId,sinkId);
+
+
+
+drop table CurrMonthMcp;
+
+select * into CurrMonthMcp from (
+  select cast(n1.externalNodeId as int) srcId, cast(n2.externalNodeId as int) sinkId, f1.month_ period,
+  (f2.prcPk-f1.prcPk) mcpPk, (f2.prcOpk-f1.prcOpk) mcpOpk
+  from FtrMcp f1, FtrMcp f2, Node n1, Node n2
+  where n1.marketId=1 and n2.marketId=1
+  and n1.nodeId=f1.nodeId and n2.nodeId=f2.nodeId
+  and f1.month_=f2.month_
+  and f1.month_='2016-01-01'
+  -- and cast(n1.externalNodeId as int)<48650 and cast(n2.externalNodeId as int)<48650
+) z;
+
+
+-- select * from CurrMonthMcp;
+create nonclustered index iCurrMonthMcp on CurrMonthMcp(srcId,sinkId);
+
+
+
+
+drop table CurrMonthRev;
+
+select * into CurrMonthRev from (
+  select cast(n1.externalNodeId as int) srcId, cast(n2.externalNodeId as int) sinkId, f1.period,
+  (f2.revDaPk-f1.revDaPk) revPk, (f2.revDaOpk-f1.revDaOpk) revOpk
+  from PriceMonthly2016 f1, PriceMonthly2016 f2, Node n1, Node n2
+  where n1.marketId=1 and n2.marketId=1
+  and n1.nodeId=f1.nodeId and n2.nodeId=f2.nodeId
+  and f1.period=f2.period
+  -- and cast(n1.externalNodeId as int)<48650 and cast(n2.externalNodeId as int)<48650
+) z;
+
+create nonclustered index iCurrMonthRev on CurrMonthRev(srcId,sinkId);
+
+
+-- select * from SummaryData;
+
+select s.srcId,s.sinkId,n1.nodeName,n1.zoneName,n2.nodeName,n2.zoneName,
+avgPk,minPk,maxPk,sdPk, round(avgPk/sdPk,2) shPk, mcpPk, revPk
+from SummaryData s, NodeZone n1, NodeZone n2, CurrMonthMcp c, CurrMonthRev r
+where n1.pNodeId=s.srcId and n2.pNodeId=s.sinkId
+and c.srcId=s.srcId and c.sinkId=s.sinkId
+and r.srcId=s.srcId and r.sinkId=s.sinkId;
+
+
+
